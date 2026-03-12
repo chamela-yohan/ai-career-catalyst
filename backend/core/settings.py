eda1195cebd4a43de72f37862d3b46974b9a1ef1
ChamelaYohan <chamelayohan@gmail.com>
2026-03-12 17:50:26 +0530

    Step 5: Initialized React with Vite, added Frontend Dockerfile and configured CORS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -43,8 +43,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]

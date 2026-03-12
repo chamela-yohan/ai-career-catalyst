8a69d36eb758d38572cbfe9f960479bc0c4c5242
ChamelaYohan <chamelayohan@gmail.com>
2026-03-12 18:47:54 +0530

    Step 7: Implemented Interview Question Generator with Prompt Engineering

--- backend/ai_assistant/urls.py
+++ backend/ai_assistant/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import GeminiTestView
+from .views import GeminiTestView,InterviewGeneratorView
+
 
 urlpatterns = [
-    path('test-ai/', GeminiTestView.as_view(), name='test_ai')
+    path('test-ai/', GeminiTestView.as_view(), name='test_ai'),
+    path('generate-questions/', InterviewGeneratorView.as_view(), name='generate_questions'),
 ]
--- backend/ai_assistant/views.py
+++ backend/ai_assistant/views.py
@@ -25,3 +25,36 @@ class GeminiTestView(APIView):
             
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class InterviewGeneratorView(APIView):
+    def post(self, request):
+        job_role = request.data.get('job_role', '')
+        experience = request.data.get('experience', 'Junior')
+
+        if not job_role:
+            return Response({"error": "Job role is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Prompt Engineering
+        prompt = f"""
+        Act as a professional technical interviewer. 
+        Generate 5 interview questions for a {job_role} position at a {experience} level.
+        Return the response ONLY as a JSON array of objects with the following keys:
+        "id" (int), "question" (string), "topic" (string).
+        Do not include any other text or explanation.
+        """
+
+        try:
+            model = genai.GenerativeModel('gemini-2.5-flash')
+            response = model.generate_content(prompt)
+            
+            # AI එකෙන් එන JSON string එක clean කරිම
+            
+            raw_text = response.text.replace('```json', '').replace('```', '').strip()
+            
+            return Response({
+                "status": "success",
+                "questions": eval(raw_text) # සරලව string එක list එකක් බවට පත් කිරීම
+            }, status=status.HTTP_200_OK)
+            
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
